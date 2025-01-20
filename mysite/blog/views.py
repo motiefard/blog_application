fe5fc8c76fc2fcd45a1a103601979d8da3fc8f1c
motiefard <mozhdemotieifard@gmail.com>
2025-01-20 16:06:20 +0330

    build class-based view instead of function

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import generic
 from .models import Post
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)     #init Pagination with 3 posts per page
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
-
+class PostListView(generic.ListView):
+    queryset = Post.published.all()
+    paginate_by = 3
+    context_object_name ='posts'
+    template_name = 'blog/post/list.html'
+    
 
 def post_detail(request, year, month, day, post):
     # try:
