ff685fc0012b7ab300efe69f1cbd30e62e2708ff
motiefard <mozhdemotieifard@gmail.com>
2025-02-02 20:59:38 +0330

    recommend post by email create and handle form with Django, send email, work with enviroment-variables

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.views import generic
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 class PostListView(generic.ListView):
     queryset = Post.published.all()
@@ -22,3 +24,36 @@ def post_detail(request, year, month, day, post):
                             publish__month=month,
                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post_data})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status= Post.Status.PUBLISHED)
+    sent = False
+    
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                # from_email=cd['email'],
+                recipient_list=[cd['to']]
+            )
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent':sent} )
